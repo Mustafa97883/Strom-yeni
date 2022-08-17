d26d290c15697606e462ad14fd2d4332d8feeef5
Glitch (colorful-potent-aftershave) <none>
2022-08-17 11:20:55 +0000

    👼🚀 Checkpoint ./komutlar/yardım-çekiliş.js:14029226/2690 ./komutlar/yardım-rütbe.js:14029226/570 ./komutlar/ticketsistem.js:14029226/583 ./komutlar/sunucutanıt.js:14029226/576 ./komutlar/sayaçkanal.js:14029226/576 ./komutlar/sayaçkapat.js:14029226/570 ./komutlar/rütbe-top.js:14029226/571 ./komutlar/rütbe-sıfırla.js:14029226/570 ./komutlar/rütbe-ekle.js:14029226/570 ./komutlar/rütbe-davetsıfırla.js:14029226/570 ./komutlar/rütbe-davetlerim.js:14029226/570 ./komutlar/rütbe-davetkanal.js:14029226/570 ./komutlar/rütbe-davetkanal-sıfırla.js:14029226/570 ./komutlar/rütbe-bonus.js:14029226/1182 ./komutlar/reklamengel.js:14029226/570 ./komutlar/otorol.js:14029226/1684 ./komutlar/muterol.js:14029226/570 ./komutlar/modlog.js:14029226/573 ./komutlar/küfürengel.js:14029226/563 ./komutlar/kurallar.js:14029226/570 ./komutlar/kick-limit.js:14029226/570 ./komutlar/kanalkoruma.js:14029226/570 ./komutlar/jailyetkilirol.js:14029226/572 ./komutlar/jailrol.js:14029226/570 ./komutlar/jaillog.js:14029226/572

--- komutlar/ticketsistem.js
+++ komutlar/ticketsistem.js
@@ -4,7 +4,10 @@ let prefix = ayarlar.prefix;
 
 
 exports.run = async (bot, msg, args, client) => {
-  
+  const DBL = require('dblapi.js')
+const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc1Njg4MzMwOTI3MDY2MzIyOSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjYwNzM0NTkyfQ.Tci7n9zVPbCAfU70t8CccDiH7lg7pGrvYHnIvRk9f1s', client)
+dbl.hasVoted(msg.author.id).then(voted => {
+      if(voted) {
   const çekiliş = new Discord.MessageEmbed()
     .setAuthor(`Strom  | Ticket Sistem`)
     .setTitle(``)
@@ -53,6 +56,12 @@ exports.run = async (bot, msg, args, client) => {
     );
   msg.channel.send(çekiliş);
 
+      
+     } else {
+        msg.channel.send(` Bu Komutu Sadece 12 Saatte Bir Oyvererek Kullanabilirsiniz Oyvermek İçin (https://top.gg/bot/756883309270663229/vote) linke Tıklayarak Oyverebilirsiniz. Oy Verdiyseniz 5 Dakka Bekleyiniz`) 
+              
+}
+        })
       
       },
 exports.conf = {
