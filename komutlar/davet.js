0a3da0e54e34f96d733736224917053a0e0d7207
Glitch (colorful-potent-aftershave) <none>
2022-08-17 10:09:49 +0000

    🐇💕 Checkpoint ./komutlar/ban.js:14029226/10356 ./komutlar/banlog.js:14029226/1213 ./komutlar/banlimit.js:14029226/1162 ./komutlar/banner.js:14029226/749 ./komutlar/basitlogo.js:14029226/746 ./komutlar/başvuru.js:14029226/2186 ./komutlar/benimki.js:14029226/636 ./komutlar/beyazliste.js:14029226/1188 ./komutlar/blacklist.js:14029226/1946 ./komutlar/bot-ekle.js:14029226/2266 ./komutlar/botlistayar.js:14029226/4508 ./komutlar/botlistkural.js:14029226/1500 ./komutlar/botlist-onayla.js:14029226/1690 ./komutlar/botlist-reddet.js:14029226/1868 ./komutlar/bugbildir.js:14029226/1439 ./komutlar/capsengel.js:14029226/1173 ./komutlar/cmd.js:14029226/934 ./komutlar/davet.js:14029226/950 ./komutlar/dinamiklogo.js:14029226/803 ./komutlar/elmas-logo.js:14029226/748 ./komutlar/everyonehereengel.js:14029226/2190 ./komutlar/fbi-gif.js:14029226/503 ./komutlar/green-logo.js:14029226/797 ./komutlar/güncelleme.js:14029226/1828 ./komutlar/hapishane.js:14029226/6472

---
+++ komutlar/davet.js
@@ -0,0 +1,31 @@
+const Strom = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+
+
+
+exports.run = (client, message, args) => {
+    const embed = new Strom.MessageEmbed() 
+        
+        .setTitle(`${client.user.username} DAVET SİSTEMİ `)
+        .setDescription(`💼 **Botun Davet Linki İçin** [TIKLA](https://discord.com/api/oauth2/authorize?client_id=756883309270663229&permissions=8&scope=bot) \n<a:hg:855392659136839680> **Destek Sunucusu İçin** [TIKLA](https://discord.gg/bqnNNzf9RC)`)
+        .setThumbnail(client.user.avatarURL)
+        .setFooter(`${message.author.username} Başarıyla ${ayarlar.prefix}davet Sistemi Kullandı!`, message.author.avatarURL)
+    .setColor(`RANDOM`)
+    return message.channel.send(embed);
+  
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
+};
+
+exports.help = {
+  name: 'davet',
+  description: '',
+  usage: 'davet'
+};
